623b9119a63c59bdffd9c5395acf30f048161e04
Is-haka <isihakamabo5@gmail.com>
2021-12-06 14:38:07 +0300

    The first commit of the AD project

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AD</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="start">
+        <div class="logo">
+            <h2 id="logo">LOGO</h2>
+        </div>
+        <div class="navigation">
+            <ul class="nav">
+                <li id="list">home</li>
+                <li id="list">contacts</li>
+                <li id="list">about</li>
+            </ul>
+</div>
+    </div>
+    <div class="welcome">
+        <div class="leftside">
+            <div class="background">
+                <p id="greeting">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab praesentium aliquid doloremque at dolores facere error quia voluptatem labore. Deserunt dignissimos voluptatum quasi earum! Ab, veniam! Culpa, magni facere!</p>
+            </div>
+        </div>
+        <div class="rightside">
+            <div class="form">
+                <form action="" method="post" id="form">
+                    <input type="text" name="" id="email" placeholder="Email address">
+                    <input type="password" name="" id="password" placeholder="Password">
+                    <input type="button" value="Sign Up" id="signup">
+                </form>
+                <span style="font-size: 14px;">Already have an account? <a href="#" >Login</a></span>
+            </div>
+        </div>
+    </div>
+
+<script src="JS/index.js"></script>
+</body>
+</html>
