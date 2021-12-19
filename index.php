5f45b394d6b1a4339b54a0bc3b164ff2cfe7a8be
Is-haka <isihakamabo5@gmail.com>
2021-12-19 22:33:23 +0300

    changing the content of the landing page

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                   <nav class="main-nav">
                       <!-- ***** Logo Start ***** -->
                       <a href="index.php" class="logo">
-                          Edu Meeting
+                      LOGO
                       </a>
                       <!-- ***** Logo End ***** -->
                       <!-- ***** Menu Start ***** -->
@@ -82,9 +82,10 @@
             <div class="row">
               <div class="col-lg-12">
                 <div class="caption">
-              <h6>Hello Students</h6>
-              <h2>Welcome to Education</h2>
-              <p>This is an edu meeting HTML CSS template provided by <a rel="nofollow" href="https://templatemo.com/page/1" target="_blank">TemplateMo website</a>. This is a Bootstrap v5.1.3 layout. The video background is taken from Pexels website, a group of young people by <a rel="nofollow" href="https://www.pexels.com/@pressmaster" target="_blank">Pressmaster</a>.</p>
+              <h6>Assalamu alykum warahmatullah wabarakatuh</h6>
+              <h4 id="whead" class="bd-red">Welcome to THAQAAFAT AL-ISLAAMIYYA</h4>
+              <p>This is our official website that we will going to use it to run some of our programs and services so as to provide our teaching using the the modern way.</p>
+              <p><em>Are you a parent or guardian of a child? if <b>"Yes"</b> please click the button below to create an account so that you can get some update in our Madaaris</em></p>
               <div class="main-button-red">
                   <div class="scroll-to-section"><a href="#contact">Join Us Now!</a></div>
               </div>
