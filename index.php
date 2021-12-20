bdcf513ba2fbd0da2d920ea6353290560af5fb03
Is-haka <isihakamabo5@gmail.com>
2021-12-21 00:53:13 +0300

    replacing content with the current project

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                       <!-- ***** Menu Start ***** -->
                       <ul class="nav">
                           <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                          <li><a href="meetings.php">Meetings</a></li>
+                          <!-- <li><a href="meetings.php">Meetings</a></li>
                           <li class="scroll-to-section"><a href="#apply">Apply Now</a></li>
                           <li class="has-sub">
                               <a href="javascript:void(0)">Pages</a>
@@ -56,7 +56,7 @@
                                   <li><a href="meetings.php">Upcoming Meetings</a></li>
                                   <li><a href="meeting-details.php">Meeting Details</a></li>
                               </ul>
-                          </li>
+                          </li> -->
                           <li class="scroll-to-section"><a href="#courses">Courses</a></li> 
                           <li class="scroll-to-section"><a href="#contact">Contact Us</a></li> 
                       </ul>        
@@ -108,8 +108,8 @@
                 <img src="assets/images/service-icon-01.png" alt="">
               </div>
               <div class="down-content">
-                <h4>Best Education</h4>
-                <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
+                <h4>Good Teaching</h4>
+                <!-- <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p> -->
               </div>
             </div>
             
@@ -118,8 +118,8 @@
                 <img src="assets/images/service-icon-02.png" alt="">
               </div>
               <div class="down-content">
-                <h4>Best Teachers</h4>
-                <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
+                <h4>Good Teachers</h4>
+                <!-- <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p> -->
               </div>
             </div>
             
@@ -128,8 +128,8 @@
                 <img src="assets/images/service-icon-03.png" alt="">
               </div>
               <div class="down-content">
-                <h4>Best Students</h4>
-                <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
+                <h4>Students Awards</h4>
+                <!-- <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p> -->
               </div>
             </div>
             
@@ -138,8 +138,8 @@
                 <img src="assets/images/service-icon-02.png" alt="">
               </div>
               <div class="down-content">
-                <h4>Online Meeting</h4>
-                <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
+                <h4>Online Follow up</h4>
+                <!-- <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p> -->
               </div>
             </div>
             
@@ -148,8 +148,8 @@
                 <img src="assets/images/service-icon-03.png" alt="">
               </div>
               <div class="down-content">
-                <h4>Best Networking</h4>
-                <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p>
+                <h4>modern madaaris</h4>
+                <!-- <p>Suspendisse tempor mauris a sem elementum bibendum. Praesent facilisis massa non vestibulum.</p> -->
               </div>
             </div>
             
@@ -159,12 +159,12 @@
     </div>
   </section>
 
-  <section class="upcoming-meetings" id="meetings">
+  <!-- <section class="upcoming-meetings" id="meetings">
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
           <div class="section-heading">
-            <h2>Upcoming Meetings</h2>
+            <h2>Upcoming Parents Meetings</h2>
           </div>
         </div>
         <div class="col-lg-4">
@@ -256,9 +256,9 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
-  <section class="apply-now" id="apply">
+  <!-- <section class="apply-now" id="apply">
     <div class="container">
       <div class="row">
         <div class="col-lg-6 align-self-center">
@@ -344,14 +344,14 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
   <section class="our-courses" id="courses">
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
           <div class="section-heading">
-            <h2>Our Popular Courses</h2>
+            <h2>What we teaches</h2>
           </div>
         </div>
         <div class="col-lg-12">
@@ -359,7 +359,7 @@
             <div class="item">
               <img src="assets/images/course-01.jpg" alt="Course One">
               <div class="down-content">
-                <h4>Morbi tincidunt elit vitae justo rhoncus</h4>
+                <h4>Quran and its translation</h4>
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
@@ -372,7 +372,7 @@
                       </ul>
                     </div>
                     <div class="col-4">
-                       <span>$160</span>
+                       <span></span>
                     </div>
                   </div>
                 </div>
@@ -381,7 +381,7 @@
             <div class="item">
               <img src="assets/images/course-02.jpg" alt="Course Two">
               <div class="down-content">
-                <h4>Curabitur molestie dignissim purus vel</h4>
+                <h4>Prophet (S.A.W) Hadith</h4>
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
@@ -392,7 +392,7 @@
                       </ul>
                     </div>
                     <div class="col-4">
-                       <span>$180</span>
+                       <span></span>
                     </div>
                   </div>
                 </div>
@@ -401,7 +401,7 @@
             <div class="item">
               <img src="assets/images/course-03.jpg" alt="">
               <div class="down-content">
-                <h4>Nulla at ipsum a mauris egestas tempor</h4>
+                <h4>Tawheed</h4>
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
@@ -413,7 +413,7 @@
                       </ul>
                     </div>
                     <div class="col-4">
-                       <span>$140</span>
+                       <span></span>
                     </div>
                   </div>
                 </div>
@@ -422,7 +422,7 @@
             <div class="item">
               <img src="assets/images/course-04.jpg" alt="">
               <div class="down-content">
-                <h4>Aenean molestie quis libero gravida</h4>
+                <h4>All praying fundamentals</h4>
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
@@ -435,7 +435,7 @@
                       </ul>
                     </div>
                     <div class="col-4">
-                       <span>$120</span>
+                       <span></span>
                     </div>
                   </div>
                 </div>
@@ -444,7 +444,7 @@
             <div class="item">
               <img src="assets/images/course-01.jpg" alt="">
               <div class="down-content">
-                <h4>Lorem ipsum dolor sit amet adipiscing elit</h4>
+                <h4>Muslims outfits</h4>
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
@@ -457,217 +457,12 @@
                       </ul>
                     </div>
                     <div class="col-4">
-                       <span>$250</span>
+                       <span></span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="item">
-              <img src="assets/images/course-02.jpg" alt="">
-              <div class="down-content">
-                <h4>TemplateMo is the best website for Free CSS</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$270</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <img src="assets/images/course-03.jpg" alt="">
-              <div class="down-content">
-                <h4>Web Design Templates at your finger tips</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$340</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <img src="assets/images/course-04.jpg" alt="">
-              <div class="down-content">
-                <h4>Please visit our website again</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$360</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <img src="assets/images/course-01.jpg" alt="">
-              <div class="down-content">
-                <h4>Responsive HTML Templates for you</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$400</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <img src="assets/images/course-02.jpg" alt="">
-              <div class="down-content">
-                <h4>Download Free CSS Layouts for your business</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$430</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <img src="assets/images/course-03.jpg" alt="">
-              <div class="down-content">
-                <h4>Morbi in libero blandit lectus cursus</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$480</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="item">
-              <img src="assets/images/course-04.jpg" alt="">
-              <div class="down-content">
-                <h4>Curabitur molestie dignissim purus</h4>
-                <div class="info">
-                  <div class="row">
-                    <div class="col-8">
-                      <ul>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                        <li><i class="fa fa-star"></i></li>
-                      </ul>
-                    </div>
-                    <div class="col-4">
-                       <span>$560</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="our-facts">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="row">
-            <div class="col-lg-12">
-              <h2>A Few Facts About Our University</h2>
-            </div>
-            <div class="col-lg-6">
-              <div class="row">
-                <div class="col-12">
-                  <div class="count-area-content percentage">
-                    <div class="count-digit">94</div>
-                    <div class="count-title">Succesed Students</div>
-                  </div>
-                </div>
-                <div class="col-12">
-                  <div class="count-area-content">
-                    <div class="count-digit">126</div>
-                    <div class="count-title">Current Teachers</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <div class="row">
-                <div class="col-12">
-                  <div class="count-area-content new-students">
-                    <div class="count-digit">2345</div>
-                    <div class="count-title">New Students</div>
-                  </div>
-                </div> 
-                <div class="col-12">
-                  <div class="count-area-content">
-                    <div class="count-digit">32</div>
-                    <div class="count-title">Awards</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> 
-        <div class="col-lg-6 align-self-center">
-          <div class="video">
-            <a href="https://www.youtube.com/watch?v=HndV87XpkWg" target="_blank"><img src="assets/images/play-icon.png" alt=""></a>
           </div>
         </div>
       </div>
@@ -687,7 +482,7 @@
                   </div>
                   <div class="col-lg-4">
                     <fieldset>
-                      <input name="name" type="text" id="name" placeholder="YOURNAME...*" required="">
+                      <input name="name" type="text" id="name" class="form-control" placeholder="YOURNAME...*" required="">
                     </fieldset>
                   </div>
                   <div class="col-lg-4">
@@ -720,19 +515,19 @@
             <ul>
               <li>
                 <h6>Phone Number</h6>
-                <span>010-020-0340</span>
+                <span>0715 641 114</span>
               </li>
               <li>
                 <h6>Email Address</h6>
-                <span>info@meeting.edu</span>
+                <span>ishaqaicon5@gmail.com</span>
               </li>
               <li>
-                <h6>Street Address</h6>
-                <span>Rio de Janeiro - RJ, 22795-008, Brazil</span>
+                <h6>Physical Address</h6>
+                <span>Arusha, Tanzania</span>
               </li>
               <li>
                 <h6>Website URL</h6>
-                <span>www.meeting.edu</span>
+                <span>www.islamedu.co.tz</span>
               </li>
             </ul>
           </div>
