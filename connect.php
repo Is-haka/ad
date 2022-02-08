9dba22fa694604603e68020a24813e9b8ef0a7a7
Is-haka <isihakamabo5@gmail.com>
2022-02-08 13:34:02 +0300

    trying to checking a remote database to see if it works

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
 
-    $db_name = "sql11470841";
-    $db_username = "sql11470841";
-    $db_userpass = "BmfClZFK16";
-    $db_host = "sql11.freemysqlhosting.net";
+    // $db_name = "sql11470841";
+    // $db_username = "sql11470841";
+    // $db_userpass = "BmfClZFK16";
+    // $db_host = "sql11.freemysqlhosting.net";
 
-    // $db_name = "ThaIs";
-    // $db_username = "root";
-    // $db_userpass = "";
-    // $db_host = "localhost";
+    $db_name = "ThaIs";
+    $db_username = "root";
+    $db_userpass = "";
+    $db_host = "localhost";
 
     $db = mysqli_connect($db_host, $db_username, $db_userpass, $db_name) or die("database connection failed");
 
