388e0643ccfaac4d18cf54990274df4635dacbd9
Is-haka <isihakamabo5@gmail.com>
2022-02-08 21:30:51 +0300

    designing a signin and registration form on landing page

--- index.php
+++ index.php
@@ -32,6 +32,8 @@
     <link rel="stylesheet" href="assets/css/templatemo-edu-meeting.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/lightbox.css">
+    <!-- <link rel="stylesheet" type="text/css" href="assets/css/sourcesanspro-font.css"> -->
+    <link rel="stylesheet" href="assets/css/style.css"/>
 
   </head>
 
@@ -91,7 +93,7 @@
               <p>This is our official website that we will going to use it to run some of our programs and services so as to provide our teaching using the the modern way.</p>
               <p><em>Are you a parent or guardian of a child? if <b>"Yes"</b> please click the button below to create an account so that you can get some update in our Madaaris</em></p>
               <div class="main-button-red">
-                  <div class="scroll-to-section"><a href="#contact">Join Us </a></div>
+                  <div class="scroll-to-section"><a href="#contact"> create account </a></div>
               </div>
           </div>
               </div>
@@ -367,13 +369,13 @@
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
-                      <ul>
+                      <!-- <ul>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
-                      </ul>
+                      </ul> -->
                     </div>
                     <div class="col-4">
                        <span></span>
@@ -389,11 +391,11 @@
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
-                      <ul>
+                      <!-- <ul>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
-                      </ul>
+                      </ul> -->
                     </div>
                     <div class="col-4">
                        <span></span>
@@ -409,12 +411,12 @@
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
-                      <ul>
+                      <!-- <ul>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
-                      </ul>
+                      </ul> -->
                     </div>
                     <div class="col-4">
                        <span></span>
@@ -430,13 +432,13 @@
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
-                      <ul>
+                      <!-- <ul>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
-                      </ul>
+                      </ul> -->
                     </div>
                     <div class="col-4">
                        <span></span>
@@ -452,13 +454,13 @@
                 <div class="info">
                   <div class="row">
                     <div class="col-8">
-                      <ul>
+                      <!-- <ul>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
                         <li><i class="fa fa-star"></i></li>
-                      </ul>
+                      </ul> -->
                     </div>
                     <div class="col-4">
                        <span></span>
@@ -476,45 +478,93 @@
   <section class="contact-us" id="contact">
     <div class="container">
       <div class="row">
-        <div class="col-lg-9 align-self-center">
-          <div class="row">
-            <div class="col-lg-12">
-              <form id="contact" action="" method="post">
-                <div class="row">
-                  <div class="col-lg-12">
-                    <h2>Let's get in touch</h2>
-                  </div>
-                  <div class="col-lg-4">
-                    <fieldset>
-                      <input name="name" type="text" id="name" class="form-control" placeholder="YOURNAME...*" required="">
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-4">
-                    <fieldset>
-                    <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="YOUR EMAIL..." required="">
-                  </fieldset>
-                  </div>
-                  <div class="col-lg-4">
-                    <fieldset>
-                      <input name="subject" type="text" id="subject" placeholder="SUBJECT...*" required="">
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                      <textarea name="message" type="text" class="form-control" id="message" placeholder="YOUR MESSAGE..." required=""></textarea>
-                    </fieldset>
-                  </div>
-                  <div class="col-lg-12">
-                    <fieldset>
-                      <button type="submit" id="form-submit" class="button">SEND MESSAGE NOW</button>
-                    </fieldset>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3">
+        <div class="col-sm-12 align-self-center">
+          <div class="row respo">
+            <div class="col-sm-5 ">
+            <div class="form-v8-content">
+			<div class="form-right">
+				<div class="tab">
+					<div class="tab-inner">
+						<button class="tablinks" onclick="openCity(event, 'sign-up')" id="defaultOpen">Sign Up</button>
+					</div>
+					<div class="tab-inner">
+						<button class="tablinks" onclick="openCity(event, 'sign-in')">Sign In</button>
+					</div>
+				</div>
+				<form class="form-detail" action="#" method="post">
+					<div class="tabcontent" id="sign-up">
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="text" name="full_name" id="full_name" class="input-text" required>
+								<span class="label">Username</span>
+		  						<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="text" name="your_email" id="your_email" class="input-text" required>
+								<span class="label">E-Mail</span>
+		  						<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="password" name="password" id="password" class="input-text" required>
+								<span class="label">Password</span>
+								<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="password" name="comfirm_password" id="comfirm_password" class="input-text" required>
+								<span class="label">Comfirm Password</span>
+								<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row-last">
+							<input type="submit" name="register" class="register" value="Register">
+						</div>
+					</div>
+				</form>
+				<form class="form-detail" action="#" method="post">
+					<div class="tabcontent" id="sign-in">
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="text" name="full_name_1" id="full_name_1" class="input-text" required>
+								<span class="label">Username</span>
+		  						<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="text" name="your_email_1" id="your_email_1" class="input-text" required>
+								<span class="label">E-Mail</span>
+		  						<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="password" name="password_1" id="password_1" class="input-text" required>
+								<span class="label">Password</span>
+								<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row">
+							<label class="form-row-inner">
+								<input type="password" name="comfirm_password_1" id="comfirm_password_1" class="input-text" required>
+								<span class="label">Comfirm Password</span>
+								<span class="border"></span>
+							</label>
+						</div>
+						<div class="form-row-last">
+							<input type="submit" name="register" class="register" value="Sign In">
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-5">
           <div class="right-info">
             <ul>
               <li>
@@ -536,6 +586,10 @@
             </ul>
           </div>
         </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
     <div class="footer">
@@ -603,6 +657,24 @@
           checkSection();
         });
     </script>
+    <script type="text/javascript">
+		function openCity(evt, cityName) {
+		    var i, tabcontent, tablinks;
+		    tabcontent = document.getElementsByClassName("tabcontent");
+		    for (i = 0; i < tabcontent.length; i++) {
+		        tabcontent[i].style.display = "none";
+		    }
+		    tablinks = document.getElementsByClassName("tablinks");
+		    for (i = 0; i < tablinks.length; i++) {
+		        tablinks[i].className = tablinks[i].className.replace(" active", "");
+		    }
+		    document.getElementById(cityName).style.display = "block";
+		    evt.currentTarget.className += " active";
+		}
+
+		// Get the element with id="defaultOpen" and click on it
+		document.getElementById("defaultOpen").click();
+	</script>
 </body>
 
 </body>
