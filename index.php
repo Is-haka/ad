b84013585545986cc66fc255dd8204060aa545c6
Is-haka <isihkamabo5@gmail.com>
2022-02-12 01:47:53 +0300

    trying to link my custom css file

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" >
     <link rel="stylesheet" href="vendor/bootstrap/css/style.css" >
+    <link rel="stylesheet" href="style.css">
 
 
     <!-- Additional CSS Files -->
