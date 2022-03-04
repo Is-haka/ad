b65c7be1bcae8e97cdd5942ed7ea10630bea256a
Is-haka <isihkamabo5@gmail.com>
2022-03-04 06:53:48 +0300

    adding a second remote database hosting

--- connect.php
+++ connect.php
@@ -1,9 +1,14 @@
 <?php 
 
-        $db_name="sql11470841";
-        $db_username="sql11470841";
-        $db_userpass="BmfClZFK16";
-        $db_host="sql11.freemysqlhosting.net";
+        // $db_name="sql11470841";
+        // $db_username="sql11470841";
+        // $db_userpass="BmfClZFK16";
+        // $db_host="sql11.freemysqlhosting.net";
+
+        $db_name = "iHP1r4udct";
+        $db_username = "iHP1r4udct";
+        $db_userpass = "Hrv8N9MZC3";
+        $db_host = "remotemysql.com";
 
         // $db_name = "ThaIs";
         // $db_username = "root";
