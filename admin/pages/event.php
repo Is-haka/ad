6ccd7c7d8a24700583aa56ac9abb38a91cd7480b
Is-haka <isihkamabo5@gmail.com>
2022-03-14 16:10:13 +0300

    finishing program the event for both post and delete processes

--- admin/pages/event.php
+++ admin/pages/event.php
@@ -119,9 +119,9 @@
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">News and Events</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Update Events</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">News and Events</h6>
+          <h6 class="font-weight-bolder mb-0">Update Events</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
         <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -182,7 +182,7 @@
                             }
                         }
                         else {
-                            echo "<span class='alert alert-sm alert-danger'>Heading can't be empty</span>";
+                            echo "<span class='badge badge-sm badge-danger text-danger'>Heading can't be empty</span>";
                         }
                     }
                 ?>
@@ -196,7 +196,8 @@
                     <textarea class="form-control" name="content" id="" cols="30" rows="10" placeholder="contents of the event"></textarea>
                   </div>
                   <div class="">
-                    <input class="btn bg-gradient-dark mb-3" href="javascript:;" type="submit" name="post" value="post">
+                    <a href="./news_event.php" class="btn btn-success btn-sm">back</a>
+                    <input class="btn btn-sm bg-gradient-info mb-3" href="javascript:;" type="submit" name="post" value="post">
                   </div>
                     </form>
                 </div>
--- admin/pages/news_event.php
+++ admin/pages/news_event.php
@@ -162,15 +162,13 @@
             <div class="card-body pt-4 p-3">
               <ul class="list-group">
                 <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                  <div class="d-flex flex-column">
+                  <div class="d-flex flex-row">
+                  <i class="material-icons text-xl me-2">note</i>
                     <h6 class="mb-3 text-sm">Oliver Liam</h6>
-                    <span class="mb-2 text-xs">Company Name: <span class="text-dark font-weight-bold ms-sm-2">Viking Burrito</span></span>
-                    <span class="mb-2 text-xs">Email Address: <span class="text-dark ms-sm-2 font-weight-bold">oliver@burrito.com</span></span>
-                    <span class="text-xs">VAT Number: <span class="text-dark ms-sm-2 font-weight-bold">FRB1235476</span></span>
                   </div>
                   <div class="ms-auto text-end">
-                    <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">delete</i>Delete</a>
-                    <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">edit</i>Edit</a>
+                  <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="#"><i class="material-icons text-sm me-2">delete</i>Delete</a>
+                  <a class="btn btn-link text-dark px-3 mb-0" href="#"><i class="material-icons text-sm me-2">edit</i>Edit</a>
                   </div>
                 </li>
               </ul>
@@ -183,20 +181,35 @@
               <h6 class="mb-0">UPDATE EVENTS</h6>
               <a class="btn bg-gradient-dark mb-3" href="event.php"><i class="material-icons text-sm">add</i>&nbsp;&nbsp;Create new event</a>
             </div>
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex p-4 mb-2 mt-3 bg-gray-100 border-radius-lg">
-                  <div class="d-flex flex-column">
-                    <h6 class="mb-3 text-sm">Ethan James</h6>
-                    <span class="mb-2 text-xs">Company Name: <span class="text-dark font-weight-bold ms-sm-2">Fiber Notion</span></span>
-                    <span class="mb-2 text-xs">Email Address: <span class="text-dark ms-sm-2 font-weight-bold">ethan@fiber.com</span></span>
-                    <span class="text-xs">VAT Number: <span class="text-dark ms-sm-2 font-weight-bold">FRB1235476</span></span>
-                  </div>
-                  <div class="ms-auto text-end">
-                    <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">delete</i>Delete</a>
-                    <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="material-icons text-sm me-2">edit</i>Edit</a>
-                  </div>
-                </li>
+            <?php
+            
+            $list_event = "SELECT * FROM events";
+            $result_event = mysqli_query($db, $list_event);
+            if ($result_event) {
+              if (mysqli_num_rows($result_event) > 0) {
+                    ?>
+                    <div class="card-body pt-4 p-3">
+                        <ul class="list-group">
+                    <?php
+                      while ($list = mysqli_fetch_assoc($result_event)) {
+                        ?>
+                            <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                              <div class="d-flex flex-row">
+                                <i class="material-icons text-xl me-2">description</i>
+                                 <h6 class="mb-3 text-sm"><?php echo $list["event_names"]; ?></h6>
+                                 </div>
+                                <div class="ms-auto text-end">
+                                <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="./delete_edit/delete.php?id='<?php echo $list["id"]; ?>'"><i class="material-icons text-sm me-2">delete</i>Delete</a>
+                                <a class="btn btn-link text-dark px-3 mb-0" href="#"><i class="material-icons text-sm me-2">edit</i>Edit</a>
+                                </div>
+                              </li>
+                        <?php
+                      }
+              }
+            }
+            
+            ?>
+            
               </ul>
             </div>
           </div>
