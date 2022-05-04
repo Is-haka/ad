d83a97eb0d52a90b61cb84694f1858d38e4171d8
Is-haka <isihkamabo5@gmail.com>
2022-05-04 15:12:28 +0300

    replacing the modified database column name

--- public/auth/register.php
+++ public/auth/register.php
@@ -5,13 +5,13 @@
 
     if (isset($_POST['register'])) {
         $fullname = mysqli_real_escape_string($db, trim(strip_tags($_POST['full_name'])));
-        $email = mysqli_real_escape_string($db, trim(strip_tags($_POST['username'])));
+        $uname = mysqli_real_escape_string($db, trim(strip_tags($_POST['username'])));
         $paddress = mysqli_real_escape_string($db, trim(strip_tags($_POST['physical_address'])));
         $gender = mysqli_real_escape_string($db, trim(strip_tags($_POST['gender'])));
         $pass = mysqli_real_escape_string($db, trim(strip_tags($_POST['password'])));
         $cpass = mysqli_real_escape_string($db, trim(strip_tags($_POST['confirm_password'])));
 
-        $result = mysqli_query($db, "SELECT * FROM users WHERE `email_address` = '$email' ");
+        $result = mysqli_query($db, "SELECT * FROM users WHERE `username` = '$uname' ");
         if ($result) {
             if (mysqli_num_rows($result) > 0) {
                 $_SESSION["exists"] = "User already exist";
@@ -19,8 +19,8 @@
             }
             elseif (mysqli_num_rows($result) < 1) {
                 if ($pass == $cpass) {
-                    $query = "INSERT INTO users(`fullname`, `email_address`, `physical_address`, `password`, `genderID`)
-                    VALUES('$fullname', '$email', '$paddress', '$pass', '$gender')";
+                    $query = "INSERT INTO users(`fullname`, `username`, `physical_address`, `password`, `genderID`)
+                    VALUES('$fullname', '$uname', '$paddress', '$pass', '$gender')";
                     $iresult = mysqli_query($db, $query);
                     if ($iresult) {
                         var_dump($iresult);
@@ -36,6 +36,9 @@
                     $_SESSION["wrongpass"] = "Password missmatched";
                     header("location: ../../index.php#contact");
                 }
+            }
+            else {
+                echo "something";
             }
         }
     }
